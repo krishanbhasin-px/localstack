6fec9af4a4403adc6c43866f7ba1824dec23a372
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-05-17 17:34:22 +0200

    refactor: move non fixtures from conftest to helperfunctions

--- tests/aws/services/events/conftest.py
+++ tests/aws/services/events/conftest.py
@@ -6,7 +6,7 @@ import pytest
 
 from localstack.utils.functions import call_safe
 from localstack.utils.strings import short_uid
-from localstack.utils.sync import retry
+from tests.aws.services.events.helper_functions import put_entries_assert_results_sqs
 
 LOG = logging.getLogger(__name__)
 
@@ -237,7 +237,7 @@ def put_events_with_filter_to_sqs(aws_client, create_sqs_events_target, clean_up
             entries = entry_asserts[0]
             for entry in entries:
                 entry["EventBusName"] = bus_name
-            message = _put_entries_assert_results_sqs(
+            message = put_entries_assert_results_sqs(
                 events_client,
                 aws_client.sqs,
                 queue_url,
@@ -257,101 +257,6 @@ def put_events_with_filter_to_sqs(aws_client, create_sqs_events_target, clean_up
             rule_name=rule_name,
             target_ids=target_id,
         )
-
-
-def _put_entries_assert_results_sqs(
-    events_client, sqs_client, queue_url: str, entries: list[dict], should_match: bool
-):
-    """
-    Put events to the event bus, receives the messages resulting from the event in the sqs queue and deletes them out of the queue.
-    If should_match is True, the content of the messages is asserted to be the same as the events put to the event bus.
-
-    :param events_client: boto3.client("events")
-    :param sqs_client: boto3.client("sqs")
-    :param queue_url: URL of the sqs queue
-    :param entries: List of entries to put to the event bus, each entry must
-                    be a dict that contains the keys: "Source", "DetailType", "Detail"
-    :param should_match:
-
-    :return: Messages from the queue if should_match is True, otherwise None
-    """
-    response = events_client.put_events(Entries=entries)
-    assert not response.get("FailedEntryCount")
-
-    def get_message(queue_url):
-        resp = sqs_client.receive_message(
-            QueueUrl=queue_url, WaitTimeSeconds=5, MaxNumberOfMessages=1
-        )
-        messages = resp.get("Messages")
-        if messages:
-            for message in messages:
-                receipt_handle = message["ReceiptHandle"]
-                sqs_client.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
-        if should_match:
-            assert len(messages) == 1
-        return messages
-
-    messages = retry(get_message, retries=5, queue_url=queue_url)
-
-    if should_match:
-        actual_event = json.loads(messages[0]["Body"])
-        if isinstance(actual_event, dict) and "detail" in actual_event:
-            assert_valid_event(actual_event)
-        return messages
-    else:
-        assert not messages
-        return None
-
-
-def assert_valid_event(event):
-    expected_fields = (
-        "version",
-        "id",
-        "detail-type",
-        "source",
-        "account",
-        "time",
-        "region",
-        "resources",
-        "detail",
-    )
-    for field in expected_fields:
-        assert field in event
-
-
-def sqs_collect_messages(
-    aws_client,
-    queue_url: str,
-    min_events: int,
-    wait_time: int = 1,
-    retries: int = 3,
-) -> list[dict]:
-    """
-    Polls the given queue for the given amount of time and extracts and flattens from the received messages all
-    events (messages that have a "Records" field in their body, and where the records can be json-deserialized).
-
-    :param queue_url: the queue URL to listen from
-    :param min_events: the minimum number of events to receive to wait for
-    :param wait_time: the number of seconds to wait between retries
-    :param retries: the number of retries before raising an assert error
-    :return: a list with the deserialized records from the SQS messages
-    """
-
-    events = []
-
-    def collect_events() -> None:
-        _response = aws_client.sqs.receive_message(QueueUrl=queue_url, WaitTimeSeconds=wait_time)
-        messages = _response.get("Messages", [])
-
-        for m in messages:
-            events.append(m)
-            aws_client.sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=m["ReceiptHandle"])
-
-        assert len(events) >= min_events
-
-    retry(collect_events, retries=retries, sleep=0.01)
-
-    return events
 
 
 @pytest.fixture
